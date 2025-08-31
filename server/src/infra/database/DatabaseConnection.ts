77f3b9da35f9a8fd180c8d172263aed8a3bcd86e
Vitor Augusto <gwtovitorpw@gmail.com>
2025-08-31 17:53:07 -0300

    fix db console

--- server/src/infra/database/DatabaseConnection.ts
+++ server/src/infra/database/DatabaseConnection.ts
@@ -6,24 +6,28 @@ export default interface DatabaseConnection{
     close():Promise<void>
 }
 
-export class PgPromiseAdapter implements DatabaseConnection{
+export class PgPromiseAdapter implements DatabaseConnection {
+    connection: any;
 
-    connection: any
-
-    constructor(dbUrl: any = null){
+    constructor(dbUrl: any = null) {
         const pgp = pgPromise();
-		this.connection = pgp(
-			process.env.DATABASE_URL || dbUrl
-		);
-        console.log("DbConnection: 🟢")
+        this.connection = pgp(process.env.DATABASE_URL || dbUrl);
+
+        this.connection.connect()
+            .then((obj: any) => {
+                console.log("DbConnection: 🟢");
+                obj.done(); 
+            })
+            .catch((err: any) => {
+                console.error("DbConnection: 🔴", err.message || err);
+            });
     }
 
     query(statement: string, params: any): Promise<any> {
-
-        return this.connection.query(statement, params)
+        return this.connection.query(statement, params);
     }
+
     async close(): Promise<void> {
         await this.connection.$pool.end();
     }
-
 }
